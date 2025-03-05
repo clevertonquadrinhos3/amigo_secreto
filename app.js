21034cf31558a86bf17c1a8ad1857a6289683d85
cleverton souza <clevertonquadrinhos3@gmail.com>
2025-03-05 17:22:22 -0300

    criação função para adicionar amigo

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let lista_de_amigos =[];
+
+function adicionarAmigo() {
+    let nome = document.getElementById('amigo').value;
+    let listaAmigos = document.getElementById('listaAmigos');
+
+    lista_de_amigos.push(nome);
+
+    let li = document.createElement('li');
+    li.textContent = nome;
+    listaAmigos.appendChild(li);
+    document.getElementById('amigo').value = ''; 
+    alert( lista_de_amigos)
+    
+}
+
