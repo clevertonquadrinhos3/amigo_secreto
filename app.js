5ad4c9394b2841e3c725f2204f33a8777e368974
cleverton souza <clevertonquadrinhos3@gmail.com>
2025-03-05 18:23:32 -0300

    Criação Função para sortear amigo

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ let lista_de_amigos =[];
 
 function adicionarAmigo() {
     let nome = document.getElementById('amigo').value;
-    let listaAmigos = document.getElementById('listaAmigos');
-    alert(nome);
-
+    let listaAmigosTela = document.getElementById('listaAmigos');
+    
     if(nome == ""){
         alert('Adicione o nome de um amigo para sortear.');
         return;
@@ -17,7 +16,26 @@ function adicionarAmigo() {
 
     let li = document.createElement('li');
     li.textContent = nome;
-    listaAmigos.appendChild(li);
+    listaAmigosTela.appendChild(li);
     document.getElementById('amigo').value = '';     
+}
+
+function sortearAmigo() {
+    
+    // Seleciona o elemento para exibir o resultado
+    let resultado = document.getElementById('resultado'); 
+    
+    // Verifica se existe ao menos uma amigo na lista. Se não houver alerta ao usuário e retorna
+    if (lista_de_amigos.length == 0) {
+        alert('Adicione pelo menos um amigo para sortear.');
+        return;
+    }
+    
+    // Sorteia o indice da lista para obter o amigo
+    let indiceSorteado = Math.floor(Math.random() * lista_de_amigos.length);
+    let amigoSorteado = lista_de_amigos[indiceSorteado];
+
+    // Atualiza o conteúdo do elemento com o amigo sorteado
+    resultado.innerHTML = `<li>Amigo sorteado: ${amigoSorteado}</li>`; 
 }
 
