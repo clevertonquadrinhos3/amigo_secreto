10ea93d0571ccf80b76c720ecedc3430082659e2
cleverton souza <clevertonquadrinhos3@gmail.com>
2025-03-05 19:15:12 -0300

    Inclusão validação nome com mensagem quando não for

--- app.js
+++ app.js
@@ -5,19 +5,37 @@ let lista_de_amigos =[];
 function adicionarAmigo() {
     let nome = document.getElementById('amigo').value;
     let listaAmigosTela = document.getElementById('listaAmigos');
+    let errorMessage = document.getElementById('error-message');
     
+    // Exibe alerta se digitar em branco para o nome
     if(nome == ""){
         alert('Adicione o nome de um amigo para sortear.');
         return;
         
     }
 
-    lista_de_amigos.push(nome);
+    
+    
+    // Regex para validar nomes (apenas letras e espaços, não aceita números ou caracteres especiaiss)
+    let nomeValido = /^[a-zA-Z\s\u00C0-\u00FF]+$/.test(nome);
 
-    let li = document.createElement('li');
-    li.textContent = nome;
-    listaAmigosTela.appendChild(li);
+    
+    // Se não for um nome válido exibe mensagem solicitando um nome válido
+    if (!nomeValido) {
+        
+        errorMessage.style.display = 'block';
+        
+    // Sendo um nome válido exibe na teal e inclui na lista de amigos para sorteio   
+    } else {
+        errorMessage.style.display = 'none';
+        let li = document.createElement('li');
+        li.textContent = nome;
+        listaAmigosTela.appendChild(li);
+        lista_de_amigos.push(nome);
+    }
+    // Independente de ser válido ou não limpa o campo para incluir um novo nome
     document.getElementById('amigo').value = '';     
+      
 }
 
 function sortearAmigo() {
